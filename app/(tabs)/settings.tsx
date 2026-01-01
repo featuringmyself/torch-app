00ee60efd00233c1b7fea0adfe9926b737165cae
Rudra <110037323+featuringmyself@users.noreply.github.com>
2026-01-01 17:17:20 +0530

    Implement settings management in SettingsScreen to load and save user preferences for torch and haptics functionality. Add Switch components for toggling these settings, enhancing user interactivity and experience.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,32 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Switch } from 'react-native';
 
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
+import { getSettings, saveSettings, type Settings } from '@/utils/settingsManager';
+import { useEffect, useState } from 'react';
 
 export default function SettingsScreen() {
+  const [settings, setSettings] = useState<Settings | null>(null);
+
+  const loadSettings = async() => {
+    const savedSettings = await getSettings();
+    setSettings(savedSettings);
+  }
+
+  useEffect(() => {
+    loadSettings();
+  }, []);
+
+  const settingsHandler = async (key: keyof Settings, value: Settings[keyof Settings]) => {
+    if (!settings) return;
+    const updatedSettings = { ...settings, [key]: value } as Settings;
+    await saveSettings(updatedSettings);
+    setSettings(updatedSettings);
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -27,7 +47,14 @@ export default function SettingsScreen() {
           Settings
         </ThemedText>
       </ThemedView>
-      <ThemedText>More features incoming...</ThemedText>
+      <ThemedView className='flex flex-row flex-1 justify-between items-center'>
+        <ThemedText>Torch enabled</ThemedText>
+        <Switch value={settings?.torchEnabled}  trackColor={{false: '#767577', true: '#81b0ff'}} thumbColor={settings?.torchEnabled ? '#f5dd4b' : '#f4f3f4'} onValueChange={() => settingsHandler('torchEnabled', !settings?.torchEnabled)} />
+      </ThemedView>
+      <ThemedView className='flex flex-row flex-1 justify-between items-center'>
+        <ThemedText>Haptics enabled</ThemedText>
+        <Switch value={settings?.hapticsEnabled}  trackColor={{false: '#767577', true: '#81b0ff'}} thumbColor={settings?.hapticsEnabled ? '#f5dd4b' : '#f4f3f4'} onValueChange={() => settingsHandler('hapticsEnabled', !settings?.hapticsEnabled)} />
+      </ThemedView>
     </ParallaxScrollView>
   );
 }
