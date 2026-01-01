b1c9bc26e9efa39338517ff9af6e8b790dabee68
Rudra <110037323+featuringmyself@users.noreply.github.com>
2026-01-01 17:47:05 +0530

    Refactor HomeScreen to improve code readability and maintainability. Update formatting for consistency, enhance permission handling, and adjust layout for torch functionality. Ensure haptic feedback is triggered based on user settings.

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { getSettings, type Settings } from "@/utils/settingsManager";
 import { CameraView, useCameraPermissions } from "expo-camera";
-import * as Haptics from 'expo-haptics';
+import * as Haptics from "expo-haptics";
 import { Image } from "expo-image";
 import { useEffect, useState } from "react";
 import { Button, Pressable, StyleSheet, View } from "react-native";
@@ -12,57 +12,69 @@ export default function HomeScreen() {
   const [torchEnabled, setTorchEnabled] = useState(true);
   const [settings, setSettings] = useState<Settings | null>(null);
 
-  const loadSettings = async() => {
+  const loadSettings = async () => {
     const savedSettings = await getSettings();
     setSettings(savedSettings);
 
-    if(savedSettings.torchEnabled !== undefined){
+    if (savedSettings.torchEnabled !== undefined) {
       setTorchEnabled(savedSettings.torchEnabled);
-    }else{
+    } else {
       setTorchEnabled(true);
     }
-  }
-
-  
+  };
 
   useEffect(() => {
     loadSettings();
   }, []);
-  const handleToggleTorch = async() => {
+  const handleToggleTorch = async () => {
     setTorchEnabled(!torchEnabled);
 
-    if(settings?.hapticsEnabled) {
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+    if (settings?.hapticsEnabled) {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     }
-  }
-  if(!permission){
-    return <View />
+  };
+  if (!permission) {
+    return <View />;
   }
 
-  if(!permission.granted){
+  if (!permission.granted) {
     return (
-     <ThemedView style={styles.container}>
-        <ThemedText type="title">Torch needs camera permission to work</ThemedText>
-        <Button onPress={requestPermission} title="grant permission"/>
-     </ThemedView>
-    )
+      <ThemedView style={styles.container}>
+        <ThemedText type="title">
+          Torch needs camera permission to work
+        </ThemedText>
+        <Button onPress={requestPermission} title="grant permission" />
+      </ThemedView>
+    );
   }
-  
+
   return (
-    // <ThemedView style={styles.container}>
-    //   <CameraView enableTorch={torchEnabled} />
-    //   <View style={styles.switchContainer}>
-    //     <Switch value={torchEnabled}  trackColor={{false: '#767577', true: '#81b0ff'}} thumbColor={torchEnabled ? '#f5dd4b' : '#f4f3f4'} onValueChange={handleToggleTorch} />
-    //   </View>
-    //   <ThemedText className="antialiased" onPress={handleToggleTorch}>
-    //     {torchEnabled ? "Torch On" : "Torch Off"}
-    //   </ThemedText>
-    // </ThemedView>
-    <View style={styles.container}>
+    <View
+      style={styles.container}
+      className={`${torchEnabled ? "bg-[#1F1A18]" : "bg-[#1E1E1E]"}`}
+    >
       <View className="h-[50vh] w-1 bg-black absolute top-0 left-1/2 -translate-x-1/2"></View>
-      <CameraView enableTorch={torchEnabled} style={{ width: 1, height: 1, position: "absolute", opacity: 0, zIndex: -1 }} />
+      <CameraView
+        enableTorch={torchEnabled}
+        style={{
+          width: 1,
+          height: 1,
+          position: "absolute",
+          opacity: 0,
+          zIndex: -1,
+        }}
+      />
       <Pressable onPress={handleToggleTorch}>
-      <Image source={torchEnabled ? require("@/assets/lightBulbOn.png") : require("@/assets/lightBulbOff.png")} className="w-10 h-10" contentFit="contain" style={{ width: 100, height: 100 }}  />
+        <Image
+          source={
+            torchEnabled
+              ? require("@/assets/lightBulbOn.png")
+              : require("@/assets/lightBulbOff.png")
+          }
+          className="w-10 h-10"
+          contentFit="contain"
+          style={{ width: 100, height: 100 }}
+        />
       </Pressable>
     </View>
   );
@@ -73,7 +85,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#1E1E1E",
   },
   switchContainer: {
     alignItems: "center",
